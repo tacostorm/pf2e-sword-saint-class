76e53beb4a1978ef234e0df5b8d2bdffc41fdc17
tacostorm <80520605+tacostorm@users.noreply.github.com>
2024-10-05 09:13:06 +0200

    Initial Commit
    creating module, adding traits to PF2e

---
+++ scripts/addTraits.js
@@ -0,0 +1,13 @@
+Hooks.once('init', function() {
+    const customWeaponTraits = {
+      "apex": "Apex",
+      "flair": "Flair",
+      "fusion": "Fusion",
+      "style": "Style",
+      "weapon-art": "Weapon Art"
+    };
+  
+    // Add these custom weapon traits to the system's existing weapon traits
+    CONFIG.PF2E.weaponTraits = mergeObject(CONFIG.PF2E.weaponTraits, customWeaponTraits);
+  });
+  

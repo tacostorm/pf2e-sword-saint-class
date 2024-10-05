639c13d729c215cf26a453ba0ad51c73d288db8e
tacostorm <80520605+tacostorm@users.noreply.github.com>
2024-10-05 23:11:46 +0200

    First Push
    Got it so editing the compendium edits the module directly
    
    Got the Destructive Fist level 1 Style _mostly_ done

--- scripts/addCompendiums.js
+++
@@ -1,9 +0,0 @@
-Hooks.once("ready", async () => {
-    // Check if the compendium is loaded
-    const pack = game.packs.get("pf2e.sword-saint-class");
-    if (pack) {
-        console.log("Sword Saint Class compendium found.");
-    } else {
-        console.warn("Sword Saint Class compendium not found!");
-    }
-});
